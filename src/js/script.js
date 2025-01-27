04240784dbb4c419d92e3586817e2d04decc16dd
Windisley <windisleyl@gmail.com>
2025-01-27 16:59:29 -0300

    Responsividade Em Todo O Projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,18 @@
-const containerslide = document.querySelector(".containerslide");
-const translateslide = document.querySelector(".translateslide");
+
+const boxtextcourt = document.querySelector(".boxtextcourt")
 
 function Scroll(){
- 
+  
+  window.addEventListener("scroll", ()=>{
+    const scrollboxtextcourt = boxtextcourt.getBoundingClientRect()
+    if(scrollboxtextcourt.top < window.innerHeight){
+        boxtextcourt.style = "opacity: 1;"
+    }else{
+      boxtextcourt.style = "opacity: 0;"
+
+    }
+
+  })
   
  
 }
