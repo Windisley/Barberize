ae04e3a711dedffda0b809c7130c48a8cc9b2427
Windisley <windisleyl@gmail.com>
2025-01-27 17:21:58 -0300

    Adicionando Animações De Scroll Da Pagina

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,35 @@
 
 const boxtextcourt = document.querySelector(".boxtextcourt")
+const boxtextcontato = document.querySelector(".boxtextcontato")
+const $form = document.querySelector(".boxcontato form")
 
 function Scroll(){
   
   window.addEventListener("scroll", ()=>{
     const scrollboxtextcourt = boxtextcourt.getBoundingClientRect()
+    const scrolltextcontato = boxtextcontato.getBoundingClientRect()
+    const scrollform = $form.getBoundingClientRect()
+
     if(scrollboxtextcourt.top < window.innerHeight){
         boxtextcourt.style = "opacity: 1;"
     }else{
       boxtextcourt.style = "opacity: 0;"
 
     }
+
+    if(scrolltextcontato.top < window.innerHeight){
+       boxtextcontato.style = "opacity: 1;"
+    }else{
+     boxtextcontato.style = "opacity: 0;"
+
+    }
+
+    if(scrollform.top < window.innerHeight){
+       $form.style = "opacity: 1;"
+   }else{
+     $form.style = "opacity: 0;"
+
+   }
 
   })
   
